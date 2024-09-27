5f6b7d0b14b6dbe913fbbb5c080416bedaa87174
valentina1298 <133918293+valentina1298@users.noreply.github.com>
2024-09-27 09:07:11 -0300

    ;k;;,k.mk.

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ function areaQuadrado(lado){
    return lado**2
 }
 console.log(areaQuadrado(6));
+
+let media = function(numero1,numero2,numero3){
+    return (numero1 + numero2 + numero3)/3;
+}
+console.log(media(1,2,3)); 
