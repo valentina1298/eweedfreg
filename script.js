bb1dea1f9c5f59b5d4ee4e6a0088f55bab5a370b
valentina1298 <133918293+valentina1298@users.noreply.github.com>
2024-09-27 09:42:48 -0300

    mv2fdfh

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ function areaQuadrado(lado){
    return lado**2
 }
 console.log(areaQuadrado(6));
+let notas=parseInt(prompt('notas primero tri?'))
 
 let media = function(numero1,numero2,numero3){
     return (numero1 + numero2 + numero3)/3;
 }
-console.log(media(1,2,3)); 
+console.log(media(1,2,3));
+console.log(media(notas,8,2));
+document.write(notas;)
