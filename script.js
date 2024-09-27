43899bfb256dbb9fc2603e4829ce5355a95378a0
valentina1298 <133918293+valentina1298@users.noreply.github.com>
2024-09-27 08:42:45 -0300

    uiu

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-documet.querSlector ('button');
-const botao =  document.querySelector ('button')
-botao.onclick = function () {
-   alert (' você clicou no botão');
-}
+
 function soma(numero1,numero2){
     return numero1 + numero2;
 }
