f3bf6787f34df253e1f6b9defd75277d956bdbc1
valentina1298 <133918293+valentina1298@users.noreply.github.com>
2024-09-27 08:41:06 -0300

    uyyi

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const botao =  document.querySelector ('button')
 botao.onclick = function () {
    alert (' você clicou no botão');
 }
-function soma (numero1,numero2){
+function soma(numero1,numero2){
     return numero1 + numero2;
 }
 console.log(soma(5,3));
