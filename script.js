321e166bbcc36ef8ca5f3cb22357152ff9ca5466
valentina1298 <133918293+valentina1298@users.noreply.github.com>
2024-09-27 08:29:22 -0300

    ttut

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const botao =  document.querySelector ('button')
 botao.onclick = function () {
    alert (' você clicou no botão');
 }
+function soma (numero1,numero2){
+    return numero1 + numero2;
+}
